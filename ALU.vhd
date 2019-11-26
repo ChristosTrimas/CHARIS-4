82fdb6d4b82c38d3c21476d0ef779b17ff4f6f58
Christos Trimas <47279694+ChristosTrimas@users.noreply.github.com>
2019-11-26 17:59:46 +0200

    Add files via upload

---
+++ ALU.vhd
@@ -0,0 +1,80 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:13:08 02/18/2019 
+-- Design Name: 
+-- Module Name:    ALU - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+use ieee.std_logic_arith.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+    Port ( A : in  STD_LOGIC_VECTOR (31 downto 0);
+           B : in  STD_LOGIC_VECTOR (31 downto 0);
+           Op : in  STD_LOGIC_VECTOR (3 downto 0);
+           Outs : out  STD_LOGIC_VECTOR (31 downto 0);
+           Zero : out  STD_LOGIC;
+           Cout : out  STD_LOGIC;
+           Ovf : out  STD_LOGIC);
+end ALU;
+
+architecture Behavioral of ALU is
+
+signal temp : STD_LOGIC_VECTOR (31 downto 0);
+signal giorgos,manousos : STD_LOGIC;
+
+begin
+
+temp <= (A + B) when Op = "0000" else
+		  (A - B) when Op = "0001" else
+		  (A AND B) when Op = "0010" else
+		  (A OR B) when Op = "0011" else
+		   (NOT A) when Op = "0100" else
+			A(31) & A(31 downto 1) when Op = "1000" else
+			'0' & A(31 downto 1) when Op = "1001" else
+			A(30 downto 0) & '0' when Op = "1010" else
+			A(30 downto 0) & A(31) when Op = "1100" else
+			A(0) & A(31 downto 1) when Op = "1101" else
+			A;
+			
+Outs <= temp after 10 ns;
+
+Zero <= '1' when temp = "00000000000000000000000000000000" else '0' after 10 ns;
+
+manousos <= A(31) XOR temp(31) when (A(31) = B(31)) AND (Op = "0000")  else
+		 B(31) XOR temp(31) when (A(31) /= B(31)) AND (Op = "0001")  else
+		 '0';
+
+Ovf <= manousos after 10 ns;
+
+giorgos <= 	A(31) when (A(31) = B(31)) AND (Op = "0000")  else
+			(NOT temp(31)) when (A(31) /= B(31)) AND (Op = "0001")  else
+			'0';
+	
+Cout <= giorgos after 10 ns;
+
+end Behavioral;
+
---
+++ MUX_5x32.vhd
@@ -0,0 +1,107 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:17:13 02/19/2019 
+-- Design Name: 
+-- Module Name:    MUX_5x32 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX_5x32 is
+    Port ( MUXRe0 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe1 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe2 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe3 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe4 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe5 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe6 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe7 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe8 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe9 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe10 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe11 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe12 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe13 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe14 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe15 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe16 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe17 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe18 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe19 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe20 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe21 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe22 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe23 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe24 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe25 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe26 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe27 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe28 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe29 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe30 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  MUXRe31 : in  STD_LOGIC_VECTOR (31 downto 0);
+           MUXsel : in  STD_LOGIC_VECTOR (4 downto 0);
+           Dout : out  STD_LOGIC_VECTOR (31 downto 0));
+end MUX_5x32;
+
+architecture Behavioral of MUX_5x32 is
+
+begin
+
+Dout <=	  MUXRe0 when MUXsel = "00000"  else
+			  MUXRe1 when MUXsel = "00001"	else
+			  MUXRe2 when MUXsel = "00010"	else
+			  MUXRe3 when MUXsel = "00011"	else
+			  MUXRe4 when MUXsel = "00100"	else
+			  MUXRe5 when MUXsel = "00101"	else  
+			  MUXRe6 when MUXsel = "00110"	else
+			  MUXRe7 when MUXsel = "00111"	else
+			  MUXRe8 when MUXsel = "01000"	else
+			  MUXRe9 when MUXsel = "01001"	else
+			  MUXRe10 when MUXsel = "01010"	else
+			  MUXRe11 when MUXsel = "01011"	else
+			  MUXRe12 when MUXsel = "01100"	else
+			  MUXRe13 when MUXsel = "01101"	else
+			  MUXRe14 when MUXsel = "01110"	else
+			  MUXRe15 when MUXsel = "01111"	else
+			  MUXRe16 when MUXsel = "10000"	else	
+			  MUXRe17 when MUXsel = "10001"	else
+			  MUXRe18 when MUXsel = "10010"	else
+			  MUXRe19 when MUXsel = "10011"	else
+			  MUXRe20 when MUXsel = "10100"	else
+			  MUXRe21 when MUXsel = "10101"	else
+			  MUXRe22 when MUXsel = "10110"	else
+			  MUXRe23 when MUXsel = "10111"	else
+			  MUXRe24 when MUXsel = "11000"	else
+			  MUXRe25 when MUXsel = "11001"	else
+			  MUXRe26 when MUXsel = "11010"	else
+			  MUXRe27 when MUXsel = "11011"	else
+			  MUXRe28 when MUXsel = "11100"	else
+			  MUXRe29 when MUXsel = "11101"	else
+			  MUXRe30 when MUXsel = "11110"	else 
+			  MUXRe31;
+
+end Behavioral;
+
